5fe0c15a2d5be0cf445b1f8ed956b078a73e20f8
magoudi <youssefmagedan@gmail.com>
2026-08-16 15:46:40 +0300

    baha

--- src/pages/EnterancePage.tsx
+++ src/pages/EnterancePage.tsx
@@ -423,7 +423,7 @@ const EnterancePage = () => {
         justifyContent: "center",
         alignItems: "center",
         minHeight: "100lvh",
-        overflow: "auto",
+        overflow: "hidden",
         background: `
           radial-gradient(ellipse at 20% 35%, rgba(252, 250, 132, 0.06) 0%, transparent 55%),
           radial-gradient(ellipse at 80% 65%, rgba(250, 247, 96, 0.04) 0%, transparent 55%),
